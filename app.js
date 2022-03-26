b32bd87d6eed8155fa6e096f045b2bf86a0fd943
Sofiane M <poubellepropre8@gmail.com>
2022-03-26 16:23:18 +0100

    first part of updating with hbs

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 
+const hbs = require('hbs');
+
+hbs.registerPartials(__dirname + '/views/partials');
+
 const app = express();
 
 app.use(express.static('public'));
 
+app.set('view engine', 'hbs');
+
+app.set('views', __dirname +'/views');
 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/views/home.html');
